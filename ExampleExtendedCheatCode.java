2cf31b96bf8b72c355d379f580b428e35a8e863e
JefreyHildebrandt <JefreyHildebrandt@gmail.com>
2017-11-22 14:09:33 -0500

    All files

---
+++ ExampleExtendedCheatCode.java
@@ -0,0 +1,12 @@
+public class ExampleExtendedCheatCode extends CheatCode
+{
+    ExampleExtendedCheatCode()
+    {
+        name = "ExampleExtendedCheatCode";
+    }
+    // 0 = defect 1 = cooperate
+    public int nextNextMove()
+    {
+        return 1;
+    }
+}
