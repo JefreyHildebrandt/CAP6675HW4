5b64bf0a50cd1ce366d4679f4ab49cad652db894
JefreyHildebrandt <JefreyHildebrandt@gmail.com>
2017-11-25 08:34:34 -0500

    Added comments

--- TeamPlayer.java
+++ TeamPlayer.java
@@ -2,21 +2,26 @@ public class TeamPlayer extends Strategy
 {
     private int tick = -1;
     private boolean isTeamPlayer = true;
+
     TeamPlayer()
     {
         name = "Team Player";
     }
+
     @Override
     public int nextMove()
     {
-        if(isTeamPlayer) {
+        if(isTeamPlayer)
+        {
             tick++;
             int tickResponse = tick - CheatCode.code.length - 2;
+            // sends out the code
             if (tick < CheatCode.code.length) {
                 return CheatCode.code[tick];
             }
             else if (tickResponse < CheatCode.responseCode.length && tickResponse >= 0)
             {
+                // verifies the opponent is CheatCode
                 if (opponentLastMove != CheatCode.responseCode[tickResponse])
                 {
                     isTeamPlayer = false;
@@ -28,6 +33,8 @@ public class TeamPlayer extends Strategy
             }
             else
             {
+                // if the opponent doesn't constantly attack, assume the opponent is not CheatCode and got lucky
+                // guessing the code
                 if(opponentLastMove != 0)
                 {
                     isTeamPlayer = false;
