b32feb7734cb5449c09b46ef1df3ba70a87b05f4
JefreyHildebrandt <JefreyHildebrandt@gmail.com>
2017-11-25 08:45:05 -0500

    Showed that setting initial opponentLastMove is needed for extending classes

--- ExampleExtendedCheatCode.java
+++ ExampleExtendedCheatCode.java
@@ -3,6 +3,7 @@ public class ExampleExtendedCheatCode extends CheatCode
     ExampleExtendedCheatCode()
     {
         name = "ExampleExtendedCheatCode";
+        opponentLastMove = 1;
     }
     // 0 = defect 1 = cooperate
     public int nextNextMove()
